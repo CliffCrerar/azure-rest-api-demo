bd01efbcd28811a9091f867ce6a79b94a6d4be5d
Cliff <cliff.crerar@gmail.com>
2019-05-01 23:11:19 +0200

    yes yes yes

--- obj/Debug/netcoreapp2.2/azure-rest-api-demo.RazorAssemblyInfo.cs
+++ obj/Debug/netcoreapp2.2/azure-rest-api-demo.RazorAssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("azure-rest-api-demo.Views")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("Azure Web App Demo.Views")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorLanguageVersionAttribute("2.1")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorConfigurationNameAttribute("MVC-2.1")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorExtensionAssemblyNameAttribute("MVC-2.1", "Microsoft.AspNetCore.Mvc.Razor.Extensions")]
